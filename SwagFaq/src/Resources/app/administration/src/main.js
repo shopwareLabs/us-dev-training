5a4034c16882d5e424bf55e1f2d3077f8e77d2ce
Malte Janz <m.janz@shopware.com>
2025-03-25 13:29:21 +0100

    feat: first working tab with card in admin product details (#3)

--- SwagFaq/src/Resources/app/administration/src/main.js
+++ SwagFaq/src/Resources/app/administration/src/main.js
@@ -1,18 +1,32 @@
 import * as sw from '@shopware-ag/meteor-admin-sdk';
 
-sw.notification.dispatch({
-    title: 'A test notification',
-    message: 'This was really easy to do'
-});
+const TAB_LOCATION ='swagfaq-product-detail-tab-faq';
+const CARD_LOCATION = 'swagfaq-product-detail-tab-faq-card';
 
+// Add the ui extensions when your extension is loaded in the hidden iFrame
 if (sw.location.is(sw.location.MAIN_HIDDEN)) {
-    sw.ui.actionButton.add({
-        action: 'swag-faq_customer-detail-action',
-        entity: 'customer',
-        view: 'detail',
-        label: 'Test action',
-        callback: (entity, entityIds) => {
-            // TODO: do something
-        },
+    await sw.ui.tabs('sw-product-detail').addTabItem({
+        label: 'FAQ',
+        componentSectionId: TAB_LOCATION
     });
+
+    await sw.ui.componentSection.add({
+        component: 'card',
+        positionId: TAB_LOCATION,
+        props: {
+            title: 'Hello from plugin',
+            subtitle: 'I am before the properties card',
+            locationId: CARD_LOCATION,
+        }
+    });
+}
+
+// Render the custom UI when the iFrame location matches your defined location
+if (sw.location.is(CARD_LOCATION)) {
+    // Todo: figure out how to use meteor and custom components
+    // Todo: maybe we can just create a plain vue app and use meteor components in there?
+    let app = document.querySelector('#app');
+    app.innerHTML = `<h1>I am the in the location "${CARD_LOCATION}"</h1>
+    <mt-button>click me</mt-button>
+    `;
 }
