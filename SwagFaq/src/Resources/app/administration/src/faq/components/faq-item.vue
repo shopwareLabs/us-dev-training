9f2ab01e6f96c550ed90a671028ab0f79305afcd
Malte Janz <m.janz@shopware.com>
2025-04-02 16:19:51 +0200

    feat: use meteor components (#4)
    * feat: use meteor components
    
    * add iframe resize

---
+++ SwagFaq/src/Resources/app/administration/src/faq/components/faq-item.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const props = defineProps(['question', 'answer']);
+</script>
+
+<template>
+    <div class="faq-item">
+        <h2>Question: {{ question }}</h2>
+        <p>Answer: {{ answer }}</p>
+    </div>
+</template>
+
+<style scoped>
+    .faq-item h2 {
+        margin: 0;
+    }
+    .faq-item {
+        margin-bottom: 16px;
+    }
+</style>
