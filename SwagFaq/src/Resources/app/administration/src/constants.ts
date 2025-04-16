14f2a44f28884125dfc5244b3b0146b14f5673a4
Malte Janz <m.janz@shopware.com>
2025-04-16 14:31:24 +0200

    feat: use entity in admin implementation (#11)
    * feat: use entity in admin implementation
    
    * code cleanup

--- SwagFaq/src/Resources/app/administration/src/constants.ts
+++ SwagFaq/src/Resources/app/administration/src/constants.ts
@@ -1,5 +1,3 @@
-export const LOCATION_CARD_CREATE =
-    'swagfaq-product-detail-tab-faq-card-create';
 export const LOCATION_CARD_LIST = 'swagfaq-product-detail-tab-faq-card-list';
 
 export const LOCATION_CMS_ELEMENT = 'swagfaq';
